d882fa5d24bd1a02529706a8b4ca2e37bd2b27a2
biozalp <biozalp@biozalp.com>
2024-02-10 22:57:29 +0300

    Initial Commit

---
+++ AirbnbReplica/AirbnbReplicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbReplicaApp.swift
+//  AirbnbReplica
+//
+//  Created by Berk Ilgar Özalp on 10.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbReplicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
