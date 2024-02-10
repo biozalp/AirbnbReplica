c8e135612ed90f8568f24b0b13281fc97d6c0846
biozalp <biozalp@biozalp.com>
2024-02-10 23:09:18 +0300

    Initial Commit

---
+++ AirbnbReplica/Core/Listings/ListingItemView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingView.swift
+//  AirbnbReplica
+//
+//  Created by Berk Ilgar Özalp on 10.02.2024.
+//
+
+import SwiftUI
+
+struct ListingItemView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ListingView()
+}
